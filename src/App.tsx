cb2b10b8bd0cee5fd743afa6ed696a4887e3cb63
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-11-04 02:58:26 +0530

    feat: add minor animaitons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from "react";
 import Dashboard from "./components/dashboard/dashboard";
 import Navbar from "./components/navbar/navbar";
 import Sidebar from "./components/sidebar/sidebar";
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        document.title = "Let's reach the Zenith! 🚀";
+      } else {
+        document.title = "AlgoZenith";
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, []);
+
   return (
     <main className="h-screen w-screen flex flex-col justify-start items-center bg-gradient-to-b from-white to-[#f1faff] px-8 pt-5 overflow-hidden">
-      <Navbar />
+      <Navbar
+        isClicked={isClicked}
+        onClick={() => {
+          setIsClicked(!isClicked);
+        }}
+      />
       <div className="w-full flex justify-between items-start">
-        <div className="w-[20%]">
-          <Sidebar />
+        <div className={isClicked ? "" : "w-[20%]"}>
+          <Sidebar toHide={isClicked} />
         </div>
-        <div className="w-[80%]">
+        <div className={isClicked ? "w-full" : "w-[80%]"}>
           <Dashboard />
         </div>
       </div>
--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -4,13 +4,17 @@ import D_sidebar from "./components/Dashboard-sidebar/D_sidebar";
 
 function Dashboard() {
   return (
-    <div className="h-screen flex flex-col justify-start items-stretch bg-white border-2 border-[#A4E6FF] p-5 rounded-md mt-[6px] mr-3">
-      <D_navbar />
-      <div className="flex justify-center items-start gap-3 w-full">
-        <div className="w-[30%]">
-          <D_sidebar />
+    <div className="h-screen">
+      <div className="h-[90%] bg-white flex flex-col justify-start items-stretch border-2 border-[#A4E6FF] p-5 rounded-md mt-[6px] mr-3">
+        <D_navbar />
+        <div className="flex justify-center items-start gap-3 w-full">
+          <div className="w-[30%]">
+            <D_sidebar />
+          </div>
+          <div className="h-screen w-full">
+            <D_content />
+          </div>
         </div>
-        <D_content />
       </div>
     </div>
   );
